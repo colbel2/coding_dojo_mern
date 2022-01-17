3378259f92fe5b279cd4f6d461087de376e691df
Colin Belmar <colbel22@gmail.com>
2022-01-17 17:15:59 -0600

    Submit a form notes

---
+++ class_notes/04-react-functional-components/02-pet-form/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PetForm from './components/PetForm';
+
+function App() {
+  return (
+    <div className="App container">
+      <PetForm></PetForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ class_notes/04-react-functional-components/02-pet-form/src/components/PetForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from  'react';
+
+const PetForm = (props) => {
+
+    let [name, setName] = useState("")
+    let [age, setAge] = useState(null)
+    let [breed, setBreed] = useState("")
+    let [hair, setHair] = useState(null)
+    return (
+        <div>
+            <h1>Make a grooming appointment for your pets</h1>
+            <form action="">
+                <div className="formgroup">
+                    <label htmlFor="">Name: </label>
+                    <input type="text" name="" id='' className='form-control' onChange={(e)=>setName(e.target.value)}/>
+                </div>
+                <div className="formgroup">
+                    <label htmlFor="">Age: </label>
+                    <input type="number" name="" id='' className='form-control' onChange={(e)=>setAge(e.target.value)}/>
+                </div>
+                <div className="formgroup">
+                    <label htmlFor="">Breed: </label>
+                    <input type="text" name="" id='' className='form-control' onChange={(e)=>setBreed(e.target.value)} />
+                </div>
+                <div className="formgroup">
+                    <label htmlFor="">Hair Length: </label>
+                    <select name="" id="" className ='form-select' onChange={(e)=>setHair(e.target.value)}>
+                        <option value="N/A">N/A</option>
+                        <option value="Less than 2">Less than 2 inches</option>
+                        <option value="More than 2 inches">More than 2 inches</option>
+                    </select>
+                </div>
+                <input type="submit" value="Reserve"/>
+            </form>
+            <hr />
+            <p>Pet Name: {name}</p>
+            <hr />
+            <p>Pet Age: {age}</p>
+            <hr />
+            <p>Pet Breed: {breed}</p>
+            <hr />
+            <p>Pet Hair Length: {hair}</p>
+        </div>
+    )
+}
+
+export default PetForm;
