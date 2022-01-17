9064eaffd73293a79042db1ebb8e2cef5013d23c
Colin Belmar <colbel22@gmail.com>
2022-01-17 12:34:21 -0600

    state notes

--- class_notes/03-class-components-menu/src/components/MenuItem.jsx
+++ class_notes/03-class-components-menu/src/components/MenuItem.jsx
@@ -2,19 +2,28 @@
 import React, { Component } from 'react'; // importing React and Compnent from React library so that we can create a React component
 
 class MenuItem extends Component { //the class name must be the same exact name of the file we just made and are working in
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: this.props.likes
+        };
+    }
     render() {
         console.log("logging this.props", this.props)
         let {name, price} = this.props 
         
         let clickLike = ()=>{
             console.log("Button Test");
-            alert("you liked it!")
+            // when the button is clicked, increase the state variable for likes
+            // this.state.likes ++; // this way does not work even though it would make sense. State variables wont change like normal variables
+            this.setState({likes: this.state.likes+1}) 
+            // alert("you liked it!")
         }
         return <div>
             <h1>{name}</h1>
             <p>Price: {price} </p>
             {this.props.children} 
-            <p>Number of likes: {this.props.likes} </p>
+            <p>Number of likes: {this.state.likes} </p>
             <button onClick = {clickLike}>Like</button>
             <div></div>
             <div></div>
