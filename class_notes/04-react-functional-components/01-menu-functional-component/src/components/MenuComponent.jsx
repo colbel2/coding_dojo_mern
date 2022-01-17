0305761ecb88dce91fdc27346cacf3c0058b70bf
Colin Belmar <colbel22@gmail.com>
2022-01-17 15:54:11 -0600

    State management in functional components use state

--- class_notes/04-react-functional-components/01-menu-functional-component/src/components/MenuComponent.jsx
+++ class_notes/04-react-functional-components/01-menu-functional-component/src/components/MenuComponent.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 const Menu = (props) => {
+    
+    // destructuring from props, the dish, price and likes
     let {food, price, likes} = props;
+
+    //creating a state variable for likes, to allow them to change. Only things in state can change
+    let [likesCount, setLikesCount] = useState(0) // the second item in the array can be named anything, however it is good practice to name it set then whatever the first word is
+
+    // a function that is attached to the like button click. Increase item by 1.
+    let likeHandler = ()=>{
+        setLikesCount(likesCount+1)
+        // console.log('Testing Button ${food}!');
+        // alert('you like ${dish}')
+
+    }
     return(
         <div>
             <h1> {food}</h1>
             <p>Price: ${price}</p>
-            <p>Likes: {likes}</p>
-            <p>Description: {props.description}{props.children}</p>
-            
+            <p>Likes: {likesCount}</p>
+            Description: {props.description}{props.children}
+            <button onClick={likeHandler} >Like</button>
         </div>
     );
 }
