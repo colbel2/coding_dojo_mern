abb7e8ee6a5e629db24c8e656f51296358cb9314
Colin Belmar <colbel22@gmail.com>
2022-01-17 21:55:10 -0600

    Console logs submit and know what was submitted. Prevented page reload

--- class_notes/04-react-functional-components/03-wall-of-ninjas/src/components/ninja.jsx
+++ class_notes/04-react-functional-components/03-wall-of-ninjas/src/components/ninja.jsx
@@ -5,8 +5,16 @@ const Ninja =(props)=>{
     let [imageUrl, setImageUrl] = useState("");
     let [favColor, setFavColor] = useState("");
 
+    const createNinja = (e)=>{
+        e.preventDefault(); // the default behavior of a form is to reload the page when submitting. This line prevents that. 
+        console.log("Submitted the form")
+        console.log(ninjaName,imageUrl,favColor)
+    }
+    
+    
+    
     return (
-        <form>
+        <form onSubmit={createNinja}>
             <div className="form-group">
                 <label htmlFor="">Ninja Name:</label>
                 <input type="text" name="" id="" className="form-control" onChange={(e)=>setNinjaName(e.target.value)}/>
