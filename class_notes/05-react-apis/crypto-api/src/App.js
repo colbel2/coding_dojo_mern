41b957536cd4ba44dbc203bb4ab848a04428ebb5
Colin Belmar <colbel22@gmail.com>
2022-01-18 12:03:06 -0600

    end of morning lecture

--- class_notes/05-react-apis/crypto-api/src/App.js
+++ class_notes/05-react-apis/crypto-api/src/App.js
@@ -1,14 +1,33 @@
 
 import './App.css';
 import CryptoCoinsUsingFetch from './components/CryptoCoinsUsingFetch';
+import CryptoCoinsUsingAxios from './components/CryptoCoinsUsingAxios';
 
 function App() {
   return (
     <div className="App">
       <h3>Hello from APP.js</h3>
-      <CryptoCoinsUsingFetch> </CryptoCoinsUsingFetch>
+      {/* <CryptoCoinsUsingFetch> </CryptoCoinsUsingFetch> */}
+      <CryptoCoinsUsingAxios></CryptoCoinsUsingAxios>
     </div>
   );
 }
 
 export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+// import CryptoCoinsUsingFetch from './components/CryptoCoinsUsingFetch';
+// import PokemonHint from './components/PokemonHint';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Crypto Api demo</h1>
+//       {/* <CryptoCoinsUsingFetch></CryptoCoinsUsingFetch> */}
+//       <PokemonHint></PokemonHint>
+//     </div>
+//   );
+// }
+
+// export default App;
