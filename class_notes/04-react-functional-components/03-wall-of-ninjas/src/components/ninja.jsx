6b609b45e8882ce508bdb620ee7e55efd3515136
Colin Belmar <colbel22@gmail.com>
2022-01-17 22:33:09 -0600

    End of morning lecture

--- class_notes/04-react-functional-components/03-wall-of-ninjas/src/components/ninja.jsx
+++ class_notes/04-react-functional-components/03-wall-of-ninjas/src/components/ninja.jsx
@@ -5,15 +5,24 @@ const Ninja =(props)=>{
     let [imageUrl, setImageUrl] = useState("");
     let [favColor, setFavColor] = useState("");
 
+    // let listOfNinjas = [];
+    // create a state variable to store every ninja submitted in form
+    let [listOfNinjas,setListOfNinjas] = useState([]);
+
     const createNinja = (e)=>{
         e.preventDefault(); // the default behavior of a form is to reload the page when submitting. This line prevents that. 
         console.log("Submitted the form")
         console.log(ninjaName,imageUrl,favColor)
+        let ninjaObj = {ninjaName:ninjaName, imageUrl:imageUrl, favColor:favColor} //you can also just do let ninjaObj = {ninjaName, imageUrl, favColor} and it will work the same way
+        console.log(ninjaObj);
+
+        setListOfNinjas([...listOfNinjas, ninjaObj])
     }
     
     
     
     return (
+        <div>
         <form onSubmit={createNinja}>
             <div className="form-group">
                 <label htmlFor="">Ninja Name:</label>
@@ -29,6 +38,22 @@ const Ninja =(props)=>{
             </div>
             <input type="submit" value="Create Ninja!" />
         </form>
+        <hr />
+        <h3>Here are our list of Ninjas!</h3>
+
+        {
+            listOfNinjas.map((ninja)=>{
+                return (
+                    <div style = {{border: "1px solid black", backgroundColor: ninja.favColor, display: "inline-block"}}>
+                        <h1>{ninja.ninjaName}</h1>
+                        <p><img src={ninja.imageUrl} alt="" width = "200px" height = "200px"/></p>
+                        <p>Favorite Color: {ninja.favColor}</p>
+                    </div> 
+                )
+            })
+        }
+        </div>
+
     )
 }
 
