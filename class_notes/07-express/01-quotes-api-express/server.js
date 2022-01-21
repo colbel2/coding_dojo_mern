bd1f83740cc0899f558ef23d50d07a3c4e33130d
Colin Belmar <colbel22@gmail.com>
2022-01-21 11:08:38 -0600

    Lecture follow along Express

--- class_notes/07-express/01-quotes-api-express/server.js
+++ class_notes/07-express/01-quotes-api-express/server.js
@@ -3,8 +3,19 @@ const app = express(); // create a variable called app. this variable is an inst
 const port = 8000; // run API on port 8000 for this class and all demos
 
 
+let quotes = [
+    {content: "it is not the mountains ahead that will wear you out, it is the pebble in your shoe", author: "Muhammad Ali"}
+    {content: "a wise man once said nothing at all", author: "wise man"}
+    {content: "Comparison is the thief of joy", author: "Theodore Roosevelt"}
+    {content: "America is built on speed, fast nasty speed", author: "Helen Keller"}
+    {content: "Fall down 7 times, get up 8", author: "Japanese Proverb"}
+]
 
 
+// this is an api endpoint
+app.get("/api/hello", (req,res)=>{
+    res.json({msg:"hello express!!!"})
+})
 
 
 
