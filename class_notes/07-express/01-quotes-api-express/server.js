d38ab10bfe35646c8c0d13d451e274f94969fd43
Colin Belmar <colbel22@gmail.com>
2022-01-21 13:03:24 -0600

    Morning lecture, algo answer, faker instructions

--- class_notes/07-express/01-quotes-api-express/server.js
+++ class_notes/07-express/01-quotes-api-express/server.js
@@ -1,20 +1,67 @@
-const express = require("express"); // require is a way to say import. This is one difference from front end to back end
-const app = express(); // create a variable called app. this variable is an instance of express.
-const port = 8000; // run API on port 8000 for this class and all demos
+const express = require("express"); //require is a way of importing something in a js file
+const app = express(); //create a variable called app--> this variable is an instance of express
+const port = 8000;
 
+//need these two lines below to be able to read and extract the information received from post requests
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) //be able to read form information
+
+
+//this is what you did in flask
+// @app.route("/hello")
+// def sayhello():
+//     return "hello"
 
 let quotes = [
-    {content: "it is not the mountains ahead that will wear you out, it is the pebble in your shoe", author: "Muhammad Ali"}
-    {content: "a wise man once said nothing at all", author: "wise man"}
-    {content: "Comparison is the thief of joy", author: "Theodore Roosevelt"}
-    {content: "America is built on speed, fast nasty speed", author: "Helen Keller"}
-    {content: "Fall down 7 times, get up 8", author: "Japanese Proverb"}
+    {content: "It is not the mountains ahead that will wear you out, it is the pebble in your shoe", author:"Muhammad Ali"},
+    {content:"A wise man once said nothing at all", author:"Wes"},
+    {content:"Comparison is a theif of joy", author:"Theodore Roosevelt"},
+    {content:"Fall down 7 times stand up 8", author:"Japanese Proverb"},
+    {content:"You Attract what you vibrate", author:"Fritz"},
+    {content:"Wherever you go, there you are", author:"Eckhart Tolle"}
 ]
 
 
-// this is an api endpoint
-app.get("/api/hello", (req,res)=>{
+//this is an api endpoint
+app.get("/api/hello",(req,res)=>{
     res.json({msg:"hello express!!!"})
+} )
+
+
+//get all quotes
+app.get("/api/quotes", (req,res)=>{
+    res.json({count: quotes.length, results: quotes })
+})
+
+//get one quote by "id"
+app.get("/api/quotes/:idx", (req,res)=>{
+    res.json({results: quotes[req.params.idx]})
+})
+
+//add a new quote
+app.post("/api/quotes", (req,res)=>{
+    console.log("req.body", req.body)
+    quotes.push(req.body) //push the form information into the quotes array
+    res.json({count: quotes.length, results: quotes })
+})
+
+
+//update a quote
+app.put("/api/quotes/:idx",(req,res)=>{
+    //two parts to a put request--> which thing we want to update (idx) and what data we want to use to update it with (req.body)
+
+    quotes[req.params.idx] = req.body //update quotes array at specified index (coming from the route) with the form information
+
+    res.json({count: quotes.length, results: quotes })
+} )
+
+
+app.delete("/api/quotes/:idx", (req,res)=>{
+    //you can use splice to remove somethign from an array
+    quotes.splice(req.params.idx,1)
+
+    res.json({counttttt: quotes.length, results: quotes })
+
 })
 
 
@@ -27,5 +74,14 @@ app.get("/api/hello", (req,res)=>{
 
 
 
-// this needs to below the other code blocks usually at the end of the file
-app.listen( port, () => console.log(`Listening on port: ${port}`) ); // accepts a port number and a call backk function
+
+
+
+
+
+
+
+
+
+//this line is usually at the bottom
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
---
+++ instructor_notes/07-express/01-quotes-api-express/server.js
@@ -0,0 +1,87 @@
+const express = require("express"); //require is a way of importing something in a js file
+const app = express(); //create a variable called app--> this variable is an instance of express
+const port = 8000;
+
+//need these two lines below to be able to read and extract the information received from post requests
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) //be able to read form information
+
+
+//this is what you did in flask
+// @app.route("/hello")
+// def sayhello():
+//     return "hello"
+
+let quotes = [
+    {content: "It is not the mountains ahead that will wear you out, it is the pebble in your shoe", author:"Muhammad Ali"},
+    {content:"A wise man once said nothing at all", author:"Wes"},
+    {content:"Comparison is a theif of joy", author:"Theodore Roosevelt"},
+    {content:"Fall down 7 times stand up 8", author:"Japanese Proverb"},
+    {content:"You Attract what you vibrate", author:"Fritz"},
+    {content:"Wherever you go, there you are", author:"Eckhart Tolle"}
+]
+
+
+//this is an api endpoint
+app.get("/api/hello",(req,res)=>{
+    res.json({msg:"hello express!!!"})
+} )
+
+
+//get all quotes
+app.get("/api/quotes", (req,res)=>{
+    res.json({count: quotes.length, results: quotes })
+})
+
+//get one quote by "id"
+app.get("/api/quotes/:idx", (req,res)=>{
+    res.json({results: quotes[req.params.idx]})
+})
+
+//add a new quote
+app.post("/api/quotes", (req,res)=>{
+    console.log("req.body", req.body)
+    quotes.push(req.body) //push the form information into the quotes array
+    res.json({count: quotes.length, results: quotes })
+})
+
+
+//update a quote
+app.put("/api/quotes/:idx",(req,res)=>{
+    //two parts to a put request--> which thing we want to update (idx) and what data we want to use to update it with (req.body)
+
+    quotes[req.params.idx] = req.body //update quotes array at specified index (coming from the route) with the form information
+
+    res.json({count: quotes.length, results: quotes })
+} )
+
+
+app.delete("/api/quotes/:idx", (req,res)=>{
+    //you can use splice to remove somethign from an array
+    quotes.splice(req.params.idx,1)
+
+    res.json({counttttt: quotes.length, results: quotes })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this line is usually at the bottom
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
