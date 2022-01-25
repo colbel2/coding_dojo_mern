dd755fb0f0e4a42282ee40dc2b500868eb70feaa
colbel2 <colbel22@gmail.com>
2022-01-25 12:03:45 -0600

    Morning lecture

--- instructor_notes/08-mongodb_mongoose/01-ninjas_belt_reviewer/server/routes/ninja.routes.js
+++ instructor_notes/08-mongodb_mongoose/01-ninjas_belt_reviewer/server/routes/ninja.routes.js
@@ -2,18 +2,32 @@
 const NinjaController = require("../controllers/ninja.controller")
 const Ninja = require("../models/ninja.model")
 
+
+//these are your backend routes---AKA api endpoints
 module.exports = (app)=>{
     app.get("/api/hello", NinjaController.sayHello)
     //more routes here
+
+    //to get all of something
     app.get("/api/ninjas", NinjaController.findAllNinjas)
 
+
+    //to create something new
     app.post("/api/ninjas", NinjaController.createNewNinja)
 
+    //find a random ninja
+    app.get("/api/ninjas/random", NinjaController.findRandomNinja)
+
+    //get one of something
     app.get("/api/ninjas/:id", NinjaController.findOneNinja)
 
+    //update one of something
     app.put("/api/ninjas/:id", NinjaController.updateNinja)
     
-    app.delete("/api/ninjas/:id", NinjaController.deleteNinja)    
+    //delete one of something
+    app.delete("/api/ninjas/:id", NinjaController.deleteNinja)
+    
+    
     
 
 }
