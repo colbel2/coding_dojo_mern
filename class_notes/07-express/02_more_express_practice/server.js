4d88d70fc6f97db2b9e8205dc4429a94ef071c57
Colin Belmar <colbel22@gmail.com>
2022-01-27 14:52:12 -0600

    created another route to display all quotes

--- class_notes/07-express/02_more_express_practice/server.js
+++ class_notes/07-express/02_more_express_practice/server.js
@@ -3,14 +3,23 @@ const app = express(); // create a variable called app. this variable is an inst
 const port = 8000; //sets port to 8000, do not set to 3000. Really no reason to change for class. 8000 and 5000 are the most common to use for backend. you could do 8001, 8002 etc. 
 
 
+let quotes = [ // lets quotes equal an array
+    {content: "It is not the mountains that will wear you out, it is the pebble in your shoe", author:"Muhammad Ali"}, // each quote object can have information stored
+    {content: "A wise man once said nothing at all", author:"unkown"},
+    {content: "Comparison is the thief of joy", author: "Theodore Roosevelt"},
+    {content: "Fall down 7 times to stand up 8", author: "Japanese Proverb"},
+    {content: "Wherver you go, there you are", author: "Eckhart Tolle"}
+]
 
+//this is an api endpoint
 app.get("/api/hello", (req,res)=>{
     res.json({msg:"hello express"}) //respond with json and return an object
 }) //accepts a route and a function. we will use an arrow function with 2 parameters. req and res. it could be named anything but the first word is a request and the 2nd is a response. common practice is req,res
 
-
-
-
+//get all quotes
+app.get("/api/quotes", (req,res)=>[
+    res.json({count: quotes.length,results: quotes}) // respond with json and return count and results
+])
 
 
 
