9f06ff7db183de3858dc14606b53459ae0c8b4bb
Colin Belmar <colbel22@gmail.com>
2022-01-27 22:59:26 -0600

    mongodb mongoose implementing models"

--- class_notes/08-mongodb-mongoose/01_ninjas_belt_reviewer/server/controllers/ninja.controller.js
+++ class_notes/08-mongodb-mongoose/01_ninjas_belt_reviewer/server/controllers/ninja.controller.js
@@ -1,3 +1,15 @@
+//controller needs to talk to models
+const Ninja = require('../models/ninja.model');
+
 module.exports.sayHello = (req,res)=>{
     res.json({msg: "hello mongoose modularized!!"})
 }
+
+module.exports.findAllNinjas = (req, res) => {
+    Ninja.find()
+        .then(allNinjas=>{
+            res.json({results: allNinjas})
+        })        
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+}
+
--- class_notes/08-mongodb-mongoose/01_ninjas_belt_reviewer/server/routes/ninja.routes.js
+++ class_notes/08-mongodb-mongoose/01_ninjas_belt_reviewer/server/routes/ninja.routes.js
@@ -13,4 +13,5 @@ const NinjaController = require("../controllers/ninja.controller")
 
 module.exports =(app)=>{
     app.get("/api/hello", NinjaController.sayHello)
+    app.get("/api/ninjas",NinjaController.findAllNinjas )
 }
