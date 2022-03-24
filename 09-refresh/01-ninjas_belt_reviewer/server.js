89e187ab67461252fa1f32668d6030c18195673d
colbel2 <colbel22@gmail.com>
2022-03-24 17:17:36 -0500

    Checkpoint: modularized application foundation

--- 09-refresh/01-ninjas_belt_reviewer/server.js
+++ 09-refresh/01-ninjas_belt_reviewer/server.js
@@ -1,26 +1,17 @@
 const express = require("express") //import express
-const mongoose = require("mongoose") //import mongoose
+
 
 const app = express(); //initialize express
 const port = 8000; // specify port in a variable
 
+//require makes it where all the code from the file u are requiring goes on this line
+require("./server/config/config")
 
+// //route for hello world for testing purposes
+// app.get("/api/hello", (req, res)=>{
+//     res.json({msg: "Hello World!"})
+// } )
 
-//connecting to our mongodb database using mongoose
-mongoose.connect('mongodb+srv://root:root@fitnessdb.owgvb.mongodb.net/ninjasdb?retryWrites=true&w=majority', { //('mongodb+srv://root:root@fitnessdb.owgvb.mongodb.net/ninjasdb?retryWrites=true&w=majority' the second root is the password I set up on database website...ninjasdb is the name i give my database
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
-
-
-
-//route for hello world for testing purposes
-app.get("/api/hello", (req, res)=>{
-    res.json({msg: "Hello World!"})
-} )
-
-
+require('./server/routes/ninja.routes')(app)
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
