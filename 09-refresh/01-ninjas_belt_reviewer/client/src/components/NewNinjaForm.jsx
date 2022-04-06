6c8e1e1968601a47c8077f34c46b50ac4eb21276
colbel2 <colbel22@gmail.com>
2022-04-06 11:37:34 -0500

    Checkpoint: data is saving to database and page does not reload after submit is entered.

--- 09-refresh/01-ninjas_belt_reviewer/client/src/components/NewNinjaForm.jsx
+++ 09-refresh/01-ninjas_belt_reviewer/client/src/components/NewNinjaForm.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 
 const NewNinjaForm = () => {
+
+    let [firstName, setFirstName] = useState("")
+    let [lastName, setLastName] = useState("")
+    let [numBelts, setNumBelts] = useState(0)
+    let [isVeteran, setIsVeteran] = useState(false)
+
+const submitHandler = (e)=> {
+    e.preventDefault();
+    console.log(firstName,lastName,numBelts,isVeteran);
+
+    let formInfoObj = {firstName:firstName, lastName:lastName, numBelts:numBelts, isVeteran:isVeteran}
+    console.log(formInfoObj)
+    axios.post("http://localhost:8000/api/create/ninja", formInfoObj)
+        .then(res=>{
+            console.log("response after post request",res)
+        })
+        .catch(err=>console.log("error in submitting post request", err))
+}
+
     return (
         <div>
-            <form>
+            <form onSubmit ={submitHandler}>
                 <div className="form-group">
                     <label htmlFor="">First Name</label>
-                    <input type="text" name="" id="" className="formcontrol" />
+                    <input onChange = {(e)=>{setFirstName(e.target.value)}} type="text" name="" id="" className="formcontrol" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Last Name</label>
-                    <input type="text" name="" id="" className="formcontrol" />
+                    <input onChange = {(e)=>{setLastName(e.target.value)}} type="text" name="" id="" className="formcontrol" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Number of Belts</label>
-                    <input type="number" name="" id="" className="formcontrol" />
+                    <input onChange = {(e)=>{setNumBelts(e.target.value)}} type="number" name="" id="" className="formcontrol" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Are You a Veteran?</label>
-                    <input type="checkbox" name="" id="" className="formcontrol" />
+                    <input onChange = {(e)=>{setIsVeteran(e.target.checked)}} type="checkbox" name="" id="" className="formcontrol" />
                 </div>
                 <input type="submit" value="Create Ninja" className="btn btn-success mt-3" />
             </form>
