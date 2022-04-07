312424cbddce70154e8651a889dc0bbe5cec0cbe
colbel2 <colbel22@gmail.com>
2022-04-07 10:51:44 -0500

    Checkpoint: Display one user page info

--- 09-refresh/01-ninjas_belt_reviewer/client/src/components/OneNinja.jsx
+++ 09-refresh/01-ninjas_belt_reviewer/client/src/components/OneNinja.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
 const OneNinja = () => {
+    const { id } = useParams();
+
+    const [ninjaDetails, setNinjaDetails] = useState({})
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/oneNinja/${id}`)
+
+            .then(res => {
+                console.log("response when making request for one ninja", res)
+                setNinjaDetails(res.data.results)
+
+            })
+            .catch(err => console.log(err))
+
+
+    }, [])
+
+
+
+
+
     return (
         <div>
-            <h1>Details about One Ninja</h1>
+            <h1>Details about {ninjaDetails.firstName}</h1>
+            <p>ID: {id}</p>
+            <p>Number of Belts: {ninjaDetails.numBelts}</p>
+            <p>Veteran: {ninjaDetails.isVeteran? "Veteran": "Not Veteran"}</p>
+
         </div>
     );
 };
